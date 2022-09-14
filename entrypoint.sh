42b18b27630baaee25bf25ce63f62176db03c61c
Bron Gondwana <brong@fastmailteam.com>
2022-09-14 13:09:36 +1000

    entrypoint: add libs

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,4 +2,4 @@
 
 service rsyslog start
 /usr/cyrus/bin/master -p /var/run/cyrus/master.pid -d -L /var/run/cyrus/log
-/srv/cyrus-docker-test-server.git/webserver.pl prefork -l http://*:8001
+perl -I /srv/cyrus-imapd.git/perl/imap /srv/cyrus-docker-test-server.git/webserver.pl prefork -l http://*:8001
