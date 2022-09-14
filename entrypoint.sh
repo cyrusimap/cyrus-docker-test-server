ab66e1365c79e4dcba917d7607ca04b33cfb20c9
Bron Gondwana <brong@fastmailteam.com>
2022-09-14 13:13:06 +1000

    entrypoint: copy files

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+install -m 755 /srv/cyrus-docker-test-server.git/cyrus.conf /etc/cyrus.conf
+install -m 755 /srv/cyrus-docker-test-server.git/imapd.conf /etc/imapd.conf
 service rsyslog start
 /usr/cyrus/bin/master -p /var/run/cyrus/master.pid -d -L /var/run/cyrus/log
 perl -I /srv/cyrus-imapd.git/perl/imap /srv/cyrus-docker-test-server.git/webserver.pl prefork -l http://*:8001
