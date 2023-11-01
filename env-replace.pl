15f654a54b4e3d3bc96f43c476eab0378758e41a
Bron Gondwana <brong@fastmailteam.com>
2023-11-01 12:30:27 -0400

    add env-replace tool to update cyrus config

---
+++ env-replace.pl
@@ -0,0 +1,15 @@
+#!/usr/bin/perl -w
+
+use File::Slurp;
+
+my $src = shift;
+my $dst = shift;
+
+my $data = read_file($src);
+
+$data =~ s/{([^}]+)}/$ENV{$1}/egs;
+
+open FH, ">$dst";
+print FH $data;
+close FH;
+chmod 0755, $dst;
