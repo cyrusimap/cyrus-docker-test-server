8f14963983afee9ea4148c09d8f32b03a05c198c
Bron Gondwana <brong@fastmailteam.com>
2023-11-01 06:13:29 -0400

    no entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,5 @@ install -m 755 /srv/cyrus-docker-test-server.git/cyrus.conf /etc/cyrus.conf
 install -m 755 /srv/cyrus-docker-test-server.git/imapd.conf /etc/imapd.conf
 service rsyslog start
 /usr/cyrus/bin/master -p /var/run/cyrus/master.pid -d -L /var/run/cyrus/log
-echo "SLEEPING"
-sleep 10;
 perl -I /srv/cyrus-imapd.git/perl/imap /srv/cyrus-docker-test-server.git/make5.pl
 perl -I /srv/cyrus-imapd.git/perl/imap /srv/cyrus-docker-test-server.git/webserver.pl prefork -l http://*:8001
