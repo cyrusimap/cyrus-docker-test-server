2ef2b884793955e709c5eec3440ff417d4b1266e
Bron Gondwana <brong@fastmailteam.com>
2023-11-01 14:33:32 -0400

    run: only set up auth if configured

--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@
 /etc/init.d/rsyslog start
 
 # set up Postfix
-if [ "X$RELAYHOST" != "" ]; then
+if [ "X$RELAYAUTH" != "X" ]; then
   echo "$RELAYHOST $RELAYAUTH" >> /etc/postfix/sasl_passwd
   postmap /etc/postfix/sasl_passwd
 fi
@@ -19,7 +19,9 @@ perl /srv/cyrus-docker-test-server.git/env-replace.pl /srv/cyrus-docker-test-ser
 /usr/cyrus/bin/master -p /var/run/cyrus/master.pid -d -L /var/run/cyrus/log
 
 # create users
-perl -I /srv/cyrus-imapd.git/perl/imap /srv/cyrus-docker-test-server.git/make5.pl
+if [ "X$SKIP_CREATE_USERS" == "X" ]; then
+  perl -I /srv/cyrus-imapd.git/perl/imap /srv/cyrus-docker-test-server.git/make5.pl
+fi
 
 # run the webserver
 perl -I /srv/cyrus-imapd.git/perl/imap /srv/cyrus-docker-test-server.git/webserver.pl prefork -l http://*:8001
