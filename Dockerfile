6a0de45eca65f2d468fb0913c627da8a8a6d444a
Bron Gondwana <brong@fastmailteam.com>
2023-11-01 06:08:16 -0400

    sleep nonsense

--- Dockerfile
+++ Dockerfile
@@ -150,6 +150,7 @@ RUN env CFLAGS="-g -W -Wall -Wextra -Werror" CONFIGOPTS=" --enable-autocreate --
 LABEL org.opencontainers.image.source="https://github.com/cyrusimap/cyrus-docker-test-server"
 
 WORKDIR /srv/cyrus-docker-test-server.git/
+RUN echo "hi"
 RUN git fetch; git rebase origin/master
 
 EXPOSE 8001
--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,7 @@ install -m 755 /srv/cyrus-docker-test-server.git/cyrus.conf /etc/cyrus.conf
 install -m 755 /srv/cyrus-docker-test-server.git/imapd.conf /etc/imapd.conf
 service rsyslog start
 /usr/cyrus/bin/master -p /var/run/cyrus/master.pid -d -L /var/run/cyrus/log
-sleep 1;
+echo "SLEEPING"
+sleep 10;
 perl -I /srv/cyrus-imapd.git/perl/imap /srv/cyrus-docker-test-server.git/make5.pl
 perl -I /srv/cyrus-imapd.git/perl/imap /srv/cyrus-docker-test-server.git/webserver.pl prefork -l http://*:8001
